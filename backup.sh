a6f9133ff7a4ef0450ca3184de0ce67048ced2fc
Max Franke <max@mumintroll.org>
2016-12-14 13:34:35 +0100

    Backup script: accept hostname for remote storage

--- backup.sh
+++ backup.sh
@@ -7,4 +7,9 @@ sqlite3 data.db ".dump" | sed 's/foreign_keys=OFF/foreign_keys=ON/g' > $name
 name=`date +"/tmp/finances_backups_%Y%m%d%H%M.tgz"`
 tar cf $name sql_backups
 gpg -e -r max $name
-scp $name.gpg pi-internal:/home/max/backups
+
+if [[ "$1" != "" ]]
+then
+    ssh $1 "mkdir -p ~/data"
+    scp $name.gpg "$1:~/data/"
+fi
