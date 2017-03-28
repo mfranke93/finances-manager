1fffceac6e6ea3daee3641893ef33be2ee2f6e46
Max Franke <max@mumintroll.org>
2017-03-28 10:51:21 +0200

    Make backup into tar.gz instead of pure tar
    Increase compression by factor 5.

--- backup.sh
+++ backup.sh
@@ -5,7 +5,7 @@ name=`date +"sql_backups/backup_%Y%m%d%H%M.sql"`
 sqlite3 ~/.finances-manager/finances.db ".dump" | sed 's/foreign_keys=OFF/foreign_keys=ON/g' > $name
 
 name=`date +"/tmp/finances_backups_%Y%m%d%H%M.tgz"`
-tar cf $name sql_backups
+tar czf $name sql_backups
 gpg -e -r max $name
 
 if [[ "$1" != "" ]]
