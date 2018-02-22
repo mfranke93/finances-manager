4bacd61dc6e1d98b3c304f691b528894da284a9c
Max Franke <max@mumintroll.org>
2018-02-22 13:57:23 +0100

    Added filters for <, <= date
    Re #46

--- src/plotting/Filters.h
+++ src/plotting/Filters.h
@@ -22,7 +22,27 @@ namespace detail {
         private:
         QDate border;
     };
+
+    template<bool equalsOk>
+    class DateLessThanOrEqual
+    {
+        public:
+        DateLessThanOrEqual(QDate border_)
+            : border(border_) {};
+        ~DateLessThanOrEqual() = default;
+
+        bool operator()(RawItem const& other)
+        {
+            if (equalsOk) return other.date <= border;
+            return other.date < border;
+        };
+
+        private:
+        QDate border;
+    };
 }
 
 using DateGreaterThan = detail::DateGreaterThanOrEqual<false>;
 using DateGreaterEqual = detail::DateGreaterThanOrEqual<true>;
+using DateLessThan = detail::DateLessThanOrEqual<false>;
+using DateLessEqual = detail::DateLessThanOrEqual<true>;
